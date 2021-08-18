2916bdbd0f52ba61e8d564fc5c1d62fc1a3eefd5
Cole Winslow <colethomaswinslow7@gmail.com>
2021-08-18 13:49:21 -0700

    Fixed all Test Questions

--- src/utils/TestTitles.js
+++ src/utils/TestTitles.js
@@ -1,8 +1,8 @@
 export const TestTitles = [
   { id: 1, testTitle: "Example Quiz (Tutorial)", questions: 3 },
   { id: 2, testTitle: "Functions Quiz", questions: 5 },
-  { id: 3, testTitle: " Loops & Recursion Quiz", questions: 5 },
-  { id: 4, testTitle: "Javascript Frameworks Quiz", questions: 5 },
-  { id: 5, testTitle: "Data Structures Quiz", questions: 5 },
-  { id: 6, testTitle: "Wrap-Up Test", questions: 15 },
+  { id: 3, testTitle: "Loops & Recursion Quiz", questions: 3 },
+  { id: 4, testTitle: "Javascript Frameworks Quiz", questions: 3 },
+  { id: 5, testTitle: "Data Types Quiz", questions: 3 },
+  { id: 6, testTitle: "Wrap-Up Test", questions: 4 },
 ];
--- src/utils/Tests.js
+++ src/utils/Tests.js
@@ -1,6 +1,7 @@
 export const Tests = [
   {
     id: 1,
+    title: "Example Quiz (Tutorial)",
     Test: [
       {
         questionText: "What Color is The Sun?",
@@ -39,7 +40,7 @@ export const Tests = [
             isCorrect: false,
           },
           {
-            answerText: "Yellow And Black",
+            answerText: "Yellow And White",
             isCorrect: true,
           },
         ],
@@ -62,94 +63,361 @@ export const Tests = [
         ],
       },
     ],
-    title: "France Quiz",
   },
   {
     id: 2,
+    title: "Functions Quiz",
     Test: [
       {
-        questionText: "What idsvsvv?",
+        questionText: "What is A function?",
         answerOptions: [
           {
-            answerText: "London",
+            answerText: "A Parameter",
             isCorrect: false,
           },
           {
-            answerText: "Paris",
+            answerText:
+              "a set of instructions bundled together to achieve a specific outcome",
             isCorrect: true,
           },
           {
-            answerText: "New York",
+            answerText: "Method for Looping through An Object",
             isCorrect: false,
           },
           {
-            answerText: "Dublin",
+            answerText: "Data Type",
             isCorrect: false,
           },
         ],
       },
       {
-        questionText: "What is the Capital of Spain?",
+        questionText: "How do you Pass Data to a Function?",
         answerOptions: [
           {
-            answerText: "London",
-            isCorrect: false,
-          },
-          {
-            answerText: "Madrid",
+            answerText: "As a parameter",
             isCorrect: true,
           },
           {
-            answerText: " New York",
+            answerText: "As a String",
             isCorrect: false,
+          },
+          {
+            answerText: "With a Javascript Framework",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "How do you Write An Arrow Function?",
+        answerOptions: [
+          {
+            answerText: "<=",
+            isCorrect: false,
+          },
+          {
+            answerText: "function DoSomething()",
+            isCorrect: false,
+          },
+          {
+            answerText: "const DoSomething = () => {}",
+            isCorrect: true,
+          },
+        ],
+      },
+      {
+        questionText: "How to get data out of a function?",
+        answerOptions: [
+          {
+            answerText: "As a String",
+            isCorrect: false,
+          },
+          {
+            answerText: "With a return statement",
+            isCorrect: true,
+          },
+          {
+            answerText: "Set A Variable ",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "How to evoke or Call a Function?",
+        answerOptions: [
+          {
+            answerText: "Functions Run Automatically",
+            isCorrect: false,
+          },
+          {
+            answerText: "Function Name followed by ()",
+            isCorrect: true,
+          },
+          {
+            answerText: "Running the Start CMD ",
+            isCorrect: true,
           },
         ],
       },
     ],
-    title: "France Quiz",
   },
   {
     id: 3,
+    title: "Loops & Recursion Quiz",
     Test: [
       {
-        questionText: "What is the Capital of france?",
+        questionText:
+          " TRUE/FALSE Is it possible to loop through Objects in Javascript?",
         answerOptions: [
           {
-            answerText: "London",
-            isCorrect: false,
-          },
-          {
-            answerText: "Paris",
+            answerText: "True",
             isCorrect: true,
           },
+
           {
-            answerText: "New York",
-            isCorrect: false,
-          },
-          {
-            answerText: "Dublin",
+            answerText: "False",
             isCorrect: false,
           },
         ],
       },
       {
-        questionText: "What is the Capital of Spain?",
+        questionText: "Which Loop isn't in Javascript?",
         answerOptions: [
           {
-            answerText: "London",
+            answerText: "for",
+            isCorrect: false,
+          },
+
+          {
+            answerText: "for/in",
             isCorrect: false,
           },
           {
-            answerText: "Madrid",
-            isCorrect: true,
+            answerText: "for/of",
+            isCorrect: false,
           },
           {
-            answerText: " New York",
+            answerText: "for/while",
+            isCorrect: true,
+          },
+        ],
+      },
+      {
+        questionText: "How Do you Increment i by one in Javascript?",
+        answerOptions: [
+          {
+            answerText: "i++",
+            isCorrect: true,
+          },
+
+          {
+            answerText: "i+++",
+            isCorrect: false,
+          },
+          {
+            answerText: "i+",
+            isCorrect: false,
+          },
+          {
+            answerText: "=+ i",
             isCorrect: false,
           },
         ],
       },
     ],
-    title: "France Quiz",
+  },
+  {
+    id: 4,
+    title: "Javascript Frameworks Quiz",
+    Test: [
+      {
+        questionText: "Which option inst a Javascript Framework?",
+        answerOptions: [
+          {
+            answerText: "Bucket.js",
+            isCorrect: true,
+          },
+          {
+            answerText: "React.js",
+            isCorrect: false,
+          },
+          {
+            answerText: "Angular",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "What is the Purpose of Javascript Frameworks?",
+        answerOptions: [
+          {
+            answerText: "Its the only way to code Javascript",
+            isCorrect: false,
+          },
+          {
+            answerText:
+              "To have a collection of JavaScript code libraries that provide developers with pre-written code for routine programming tasks.",
+            isCorrect: true,
+          },
+          {
+            answerText: " To Write Unit Tests",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "What is The other Alternative to using a Framework?",
+        answerOptions: [
+          {
+            answerText: "There Are no other ways to code Javascript",
+            isCorrect: false,
+          },
+          {
+            answerText: "Database",
+            isCorrect: true,
+          },
+          {
+            answerText: "Vanilla Javascript",
+            isCorrect: true,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Data Types Quiz",
+    Test: [
+      {
+        questionText: "How Many Data types are there in Javascript?",
+        answerOptions: [
+          {
+            answerText: "2",
+            isCorrect: false,
+          },
+          {
+            answerText: "5",
+            isCorrect: false,
+          },
+          {
+            answerText: "6",
+            isCorrect: true,
+          },
+          {
+            answerText: "20",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "What Are the Data Types In Javascript?",
+        answerOptions: [
+          {
+            answerText:
+              "Numbers, Strings, Booleans, null, undefined, and Objects",
+            isCorrect: true,
+          },
+          {
+            answerText: "Deltas, Arrays, Symbols ",
+            isCorrect: false,
+          },
+          {
+            answerText: " Integer, Number, String",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "TRUE/FALSE undefined and null are Equal?",
+        answerOptions: [
+          {
+            answerText: "False",
+            isCorrect: true,
+          },
+          {
+            answerText: "True",
+            isCorrect: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Wrap-Up Test",
+    Test: [
+      {
+        questionText: "Javascript is What Type Of Programming?",
+        answerOptions: [
+          {
+            answerText: "Functional Programming",
+            isCorrect: false,
+          },
+          {
+            answerText: "Object-oriented Programming",
+            isCorrect: true,
+          },
+          {
+            answerText: "Logic Programming",
+            isCorrect: false,
+          },
+          {
+            answerText: "Pascal Programming",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "What Are the Data Types In Javascript?",
+        answerOptions: [
+          {
+            answerText:
+              "Numbers, Strings, Booleans, null, undefined, and Objects",
+            isCorrect: true,
+          },
+          {
+            answerText: "Deltas, Arrays, Symbols ",
+            isCorrect: false,
+          },
+          {
+            answerText: " Integer, Number, String",
+            isCorrect: false,
+          },
+        ],
+      },
+      {
+        questionText: "How to manipulate Arrays?",
+        answerOptions: [
+          {
+            answerText: "functions",
+            isCorrect: false,
+          },
+          {
+            answerText: "You cant In Javascript",
+            isCorrect: false,
+          },
+          {
+            answerText: "Using Array Methods",
+            isCorrect: true,
+          },
+        ],
+      },
+      {
+        questionText: "Which option inst a Javascript Framework?",
+        answerOptions: [
+          {
+            answerText: "Bucket.js",
+            isCorrect: true,
+          },
+          {
+            answerText: "React.js",
+            isCorrect: false,
+          },
+          {
+            answerText: "Angular",
+            isCorrect: false,
+          },
+        ],
+      },
+    ],
   },
 ];
